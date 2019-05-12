01d88e3c6b01695410262ce8ef10eba3b5cf90bc
FakeSam <50539070+FakeSam@users.noreply.github.com>
2019-05-11 23:33:54 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,29 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
-const token = 'NTc2OTE1MDk5Nzg1MzYzNDY2.XNde1A.PL4YNNw46uOs_R-Htibb0Lg6KaA';
-bot.login(token);
+
+const client = new Discord.Client();
+
+var BOT_TOKEN = "NTc2OTE1MDk5Nzg1MzYzNDY2.XNde1A.PL4YNNw46uOs_R-Htibb0Lg6KaA"
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.BOT_TOkEN);
